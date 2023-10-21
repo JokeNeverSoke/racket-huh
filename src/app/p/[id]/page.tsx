cb7ae4adae4d05b80c55829c5adf7309d75b01af
Joseph Zeng <jokeneversoke@gmail.com>
2023-10-20 20:57:27 -0400

    add postmessage

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -3,9 +3,18 @@
 import { api } from "@/trpc/react";
 
 import { CodeSnippet } from "@/components/code-snippet";
+import { useEffect } from "react";
 
 export default function CodePage({ params }: { params: { id: string } }) {
   const res = api.post.get.useQuery({ id: parseInt(params.id) });
+  useEffect(() => {
+    window.addEventListener("message", (e) => {
+      if (e.data === "getHeight") {
+        const height = document.body.scrollHeight;
+        window.parent.postMessage({ height }, "*");
+      }
+    });
+  });
   if (res.isLoading) {
     return <div>Loading...</div>;
   }
