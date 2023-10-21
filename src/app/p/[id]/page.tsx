903ba084676bcb75f708f61be3c3c909607018a1
Joseph Zeng <jokeneversoke@gmail.com>
2023-10-20 21:00:17 -0400

    ok

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -9,9 +9,14 @@ export default function CodePage({ params }: { params: { id: string } }) {
   const res = api.post.get.useQuery({ id: parseInt(params.id) });
   useEffect(() => {
     window.addEventListener("message", (e) => {
-      if (e.data === "getHeight") {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (e.data?.type === "getHeight") {
         const height = document.body.scrollHeight;
-        window.parent.postMessage({ height }, "*");
+        window.parent.postMessage(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+          { height, type: "setHeight", id: e.data.id },
+          "*",
+        );
       }
     });
   });
